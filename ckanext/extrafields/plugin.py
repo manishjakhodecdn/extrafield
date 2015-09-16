bb082eeac74c285a3901148074c280e71cb0008b
Manish Jakhode <manishjakhode@cdn72.(none)>
2015-09-16 21:44:13 +0530

    vocab tags

--- ckanext/extrafields/plugin.py
+++ ckanext/extrafields/plugin.py
@@ -26,18 +26,89 @@ def check_group_availability(valueItem,sufix):
                    'session': model.Session,
                    'ignore_auth': True
                 }
+    valueItem = valueItem.replace(' ', '-')
+    valueItem = valueItem.lower()
     complexdata = { 'id': valueItem+sufix }
-    groupshow  = logic.get_action('group_show')(context2, complexdata) 
+    print "complex data"
+    print complexdata
+
+    if sufix == '-dm':
+        groupshow  = logic.get_action('group_show')(context2, complexdata)
+    elif sufix == '-ph': 
+        groupshow  = logic.get_action('organization_show')(context2, complexdata)
+        
     return groupshow
+
+def manage_vocab(key, data, errors, context):
+    # check vocab available
+    # if not then create
+    # return vocab id
+    vocab_context = {'model': model,
+                   'session': model.Session,
+                   'ignore_auth': True }
+    vocab_list = []
+    value = data.get(key)
+    print "value ->" + value
+
+    if value != '' and value is not None: 
+        array = value.split(',')
+        for tag_name in array:     
+            print "manage vocab -> " + value
+            try:
+                # get vocab details
+                vocab_list = logic.get_action('vocabulary_list')(vocab_context)
+                vocab_list = vocab_list[0]
+                print "under try"
+                print vocab_list
+            except Exception, e:
+                # create Vocab
+                print "creating vocab"
+                domaindata = {'name': 'gsk_vocab'}
+                vocab_list = logic.get_action('vocabulary_create')(vocab_context, domaindata)
+       
+            # Add tag to vocab
+            print vocab_list['id']
+            # check tag already exist in vocab list
+            print "must run"
+            tag_name = tag_name.replace(' ', '-')
+            tag_name = tag_name.lower()
+            tagDict1 = {'vocabulary_id':vocab_list['id'], 'query': tag_name }
+            print tagDict1 
+            taginfo_upper = logic.get_action('tag_search')(vocab_context, tagDict1)
+            print taginfo_upper
+            print taginfo_upper['count']
+            
+            if taginfo_upper['count'] == 0: 
+                print "create tag"
+                tagdata = {'name': tag_name, 'vocabulary_id': vocab_list['id']}
+                tagdata_str = logic.get_action('tag_create')(vocab_context, tagdata)
+                print tagdata_str   
+            print vocab_list
+    return 
 
 def create_missing_group(valueItem ,sufix): 
     context1 = {'model': model,
                    'session': model.Session,
                    'ignore_auth': True
                 }
-    groupCreaated = { 'name' : valueItem+sufix , 'title' : valueItem, 'users': [{ 'capacity': 'admin', 'name' : 'admin' }], 'type': 'group' }
-    group_created_string = logic.get_action('group_create')(context1, groupCreaated)
-    return group_created_string 
+    _name = valueItem.replace(' ', '-')
+    _name = _name.lower()
+    print "this is new value item"
+    print _name
+
+    if sufix == '-dm':             
+        groupCreaated = { 'name' :  _name+sufix , 'title' : valueItem, 'users': [{ 'capacity': 'admin', 'name' : 'admin' }], 'type': 'group', 'extras':[{ 'key' : 'gsk_type', 'value' :sufix }] }
+        print "domain name data"
+        print groupCreaated
+        group_created_string = logic.get_action('group_create')(context1, groupCreaated)
+
+    elif sufix == '-ph':
+        groupCreaated1 = { 'name' : _name+sufix , 'title' : valueItem, 'users': [{ 'capacity': 'admin', 'name' : 'admin' }], 'type': 'organization', 'extras':[{ 'key' : 'gsk_type', 'value' :sufix }] }
+        print "phase name data"
+        print groupCreaated1
+        group_created_string = logic.get_action('organization_create')(context1, groupCreaated1)
+        
+    return group_created_string
 
 def _check_tags(key, data, errors, context):
 
@@ -46,7 +117,6 @@ def _check_tags(key, data, errors, context):
     keyvalue = key[0]
     pkg_name = unflattened.get('name')
     value = data.get(key)
-    print 'Value ->'+value
     if value != '' and value is not None: 
         array = value.split(',')
         for valueItem in array:     
@@ -57,26 +127,90 @@ def _check_tags(key, data, errors, context):
                 # If group is not exist then create
                 getSting = create_missing_group(valueItem, option[keyvalue])
             
-            ds_groups.append({ 'name' : valueItem+option[keyvalue] })
+            _name = valueItem.replace(' ', '-')
+            _name = _name.lower()
+            ds_groups.append({ 'name' : _name+option[keyvalue] })
+            #manage_vocab(valueItem)
+
     global set_flag
     set_flag = True
-    print ds_groups
-    print set_flag
-            
+ 
+def create_association_group(pkg_dict):
+    global set_flag 
+    global ds_groups
+
+    print pkg_dict
+    print "i am in crete"
+    phase_value = pkg_dict['phase']
+    if phase_value != '' and phase_value is not None:
+        phase_value = pkg_dict['phase']+'-ph'
+    print phase_value
+
+    context_pkg = {'model': model, 'session': model.Session, 'ignore_auth': True, 'allow_partial_update' : True }
+    packageUpdate  =  { 'id' : pkg_dict['id'], 'author' : 'gsk' , 'groups' : ds_groups, 'owner_org': phase_value }
+    packageUpdate_str = logic.get_action('package_update')(context_pkg, packageUpdate)
+    ds_groups = []
+    return
 
 
 def update_association_group(pkg_dict):
     global set_flag 
     global ds_groups
-    print ds_groups
-    print set_flag
+    print pkg_dict
     if set_flag is not False:
-        set_flag = False        
+        set_flag = False
+        phase_value = ''
         context_pkg = {'model': model, 'session': model.Session, 'ignore_auth': True, 'allow_partial_update' : True }
-        packageUpdate  =  { 'id' : pkg_dict['id'], 'notes' : 'abc' , 'groups' : ds_groups}
+        try:
+            pkg_dict['phase']
+        except Exception, e:
+            pass
+        else:
+            phase_value = pkg_dict['phase']
+            print "i am in update"
+            if phase_value != '' and phase_value is not None:
+                phase_value = pkg_dict['phase']+'-ph'
+                phase_value = phase_value.replace(' ', '-')
+                phase_value = phase_value.lower()
+            print phase_value
+        
+
+        packageUpdate  =  { 'id' : pkg_dict['id'], 'author' : 'gsk' , 'groups' : ds_groups , 'owner_org': phase_value  }
         packageUpdate_str = logic.get_action('package_update')(context_pkg, packageUpdate)
         ds_groups = []
     return
+
+def gettag_list():
+    context_tags = {'model': model,
+                   'session': model.Session,
+                   'ignore_auth': True
+                }
+    try:
+        # get vocab details
+        vocab_list = logic.get_action('vocabulary_list')(context_tags)
+        vocab_list = vocab_list[0]
+        print vocab_list 
+        print vocab_list['id']
+
+        tagDict = { 'vocabulary_id' : vocab_list['id'] }
+        print tagDict
+        gettag_list = logic.get_action('tag_list')(context_tags, tagDict)
+        gettag_list = {'result':gettag_list}
+        
+        print "i am in getlist "
+
+        return gettag_list
+    except Exception, e:
+        print " e not found users"
+        return None
+        #print "i am in taglisting"
+        #print taglist['name']
+        #tags_dict = { 'vocabulary_id': taglist['vocabulary_id'] , 'all_fields' : True }                
+        #taglisting = logic.get_action('tag_list')(context_tags, tags_dict)
+        #print taglisting
+        #data_dict = { 'vocabulary_id': taglist['id'] }
+        #tag_codes = taglisting(data_dict)
+
 
 class ExtrafieldsPlugin(plugins.SingletonPlugin,toolkit.DefaultDatasetForm):
     
@@ -84,6 +218,7 @@ class ExtrafieldsPlugin(plugins.SingletonPlugin,toolkit.DefaultDatasetForm):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IPackageController, inherit=True)
     plugins.implements(plugins.IDomainObjectModification, inherit=True)
+    plugins.implements(plugins.ITemplateHelpers)
     
     def before_view(self, pkg_dict):
         #Just to test the method
@@ -92,8 +227,12 @@ class ExtrafieldsPlugin(plugins.SingletonPlugin,toolkit.DefaultDatasetForm):
 
     def after_create(self, context, pkg_dict):
         print 'after_create'
-        update_association_group(pkg_dict)
+        create_association_group(pkg_dict)
         return
+    def get_helpers(self):
+        # Tag listing 
+        return {'gettag_list' : gettag_list }
+
 
     def after_update(self, context, pkg_dict):
         print "after_update"
@@ -130,19 +269,19 @@ class ExtrafieldsPlugin(plugins.SingletonPlugin,toolkit.DefaultDatasetForm):
                 'business_owner' : [ toolkit.get_validator('ignore_missing'), toolkit.get_converter('convert_to_extras')]
                 })
         schema.update({
-                'frontend_app' : [ toolkit.get_validator('ignore_missing'), toolkit.get_converter('convert_to_extras')]
+                'frontend_app' : [ toolkit.get_validator('ignore_missing'), toolkit.get_converter('convert_to_extras')('gettag_list'),manage_vocab]
                 })
         schema.update({
                 'business_owner' : [ toolkit.get_validator('ignore_missing'), toolkit.get_converter('convert_to_extras')]
                 })
         schema.update({
-                'domain' : [ toolkit.get_validator('ignore_missing'), toolkit.get_converter('convert_to_extras'),_check_tags]
+                'domain' : [ toolkit.get_validator('ignore_missing'), toolkit.get_converter('convert_to_extras'),_check_tags,manage_vocab]
                 })
         schema.update({
-                'key_entities' : [ toolkit.get_validator('ignore_missing'), toolkit.get_converter('convert_to_extras')]
+                'key_entities' : [ toolkit.get_validator('ignore_missing'), toolkit.get_converter('convert_to_extras'),manage_vocab]
                 })
         schema.update({
-                'phase' : [ toolkit.get_validator('ignore_missing'), toolkit.get_converter('convert_to_extras'),_check_tags]
+                'phase' : [ toolkit.get_validator('ignore_missing'), toolkit.get_converter('convert_to_extras'),_check_tags,manage_vocab]
                 })
         schema.update({
                 'chameleon_id' : [ toolkit.get_validator('ignore_missing'), toolkit.get_converter('convert_to_extras')]
@@ -212,7 +351,7 @@ class ExtrafieldsPlugin(plugins.SingletonPlugin,toolkit.DefaultDatasetForm):
                             toolkit.get_validator('ignore_missing')]
         })
         schema.update({
-            'frontend_app': [toolkit.get_converter('convert_from_extras'),
+            'frontend_app': [toolkit.get_converter('convert_from_extras')('gettag_list'),
                             toolkit.get_validator('ignore_missing')]
         })
         schema.update({
